5acfb9297f960e04df3a8591be9014b98f04bc98
otavio2110 <otaviocosta23212@gmail.com>
2023-11-23 17:29:24 -0300

    cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,26 +1,31 @@
-import cors from 'cors';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import cors from '@fastify/cors';
 import { insertRoutes } from './routes';
-// import { insertCouseRoutes } from './routes/courses/course.routes';
 import insertSchemas from './schemas';
-import app from './server'
+import app from './server';
 
-app.use(cors());
+app.register(cors, { 
+  // put your options here
+});
 
+app.addHook('preHandler', async (request: FastifyRequest, reply: FastifyReply) => {
+  console.log(`${request.method} - ${request.url}`);
+});
 
-async function main(){
-  try{
-    await insertSchemas(app)
-    await insertRoutes(app)
-    // await insertCouseRoutes(app)
+async function main() {
+  try {
+    await insertSchemas(app);
+    await insertRoutes(app);
+
     app.listen({
-      host:"0.0.0.0",
-      port:5000
-    }).then(()=>{
-      console.log("server listening on port ", 5000);
-    })
-  }catch(e){
-    console.log(e)
+      host: '0.0.0.0',
+      port: 5000
+    }).then(() => {
+      console.log('Server listening on port', 5000);
+    });
+  } catch (e) {
+    console.log(e);
   }
 }
 
-main()
+main();
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,23 +1,25 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
-import cookie from '@fastify/cookie'
-import { env } from './env'
-import userRoutes from './routes/user/user.routes'
+import { FastifyReply, FastifyRequest } from 'fastify';
+import cookie from '@fastify/cookie';
+import cors from '@fastify/cors';
+import { env } from './env';
+import userRoutes from './routes/user/user.routes';
 
-const app = require('fastify')()
+const app = require('fastify')();
 
+app.addHook('preHandler', async (request: FastifyRequest, reply: FastifyReply) => {
+  console.log(`${request.method} - ${request.url}`);
+});
 
-app.addHook('preHandler', async (request: FastifyRequest, reply: FastifyReply)=>{
-  console.log(`${request.method} - ${request.url}`)
-})
+// app.register(cors); // Register the fastify-cors plugin
 
-app.get('/healthcheck', async (request: FastifyRequest, reply: FastifyReply) =>{
-  reply.code(200).send('Everything is good!')
-})
+app.get('/healthcheck', async (request: FastifyRequest, reply: FastifyReply) => {
+  reply.code(200).send('Everything is good!');
+});
 
-app.register(cookie)
+app.register(cookie);
 
-app.register(require('@fastify/jwt'),{
-  secret: env.JWTTOKEN
-})
+app.register(require('@fastify/jwt'), {
+  secret: env.JWTTOKEN,
+});
 
-export default app
+export default app;
