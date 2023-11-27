66333a7ffc4490d4bccc0de55dbbcfd861e0b20e
julioSCassol <065201@urisantiago.br>
2023-11-27 16:18:18 -0300

    Mais css e header fazendo o minimo (inutil)

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,22 +1,33 @@
 // src/components/Header/Header.js
 import React from 'react';
 import { useAuth } from '../../AuthContext';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
 
 const Header = ({ onLoginClick }) => {
   const { user } = useAuth();
   const isTeacher = user ? user.isTeacher : false; // Verifique se user não é null antes de destruturar
+  const location = useLocation();
 
   const homepagePath = isTeacher ? '/professor-home' : '/aluno-home';
+
+  const handleHomepageClick = () => {
+    if (user) {
+      if (location.pathname === homepagePath) {
+        alert('Você já está na HomePage.');
+      } else {
+        window.location.href = homepagePath;
+      }
+    } else {
+      alert('Você não está registrado. Faça o login para acessar a HomePage.');
+    }
+  };
 
   return (
     <div className="header">
       <h1>Virtual Classmaster</h1>
       <div className="header-buttons">
-        <Link to={homepagePath} className="homepage-link">
-          <button>Ir para a HomePage</button>
-        </Link>
+        <button onClick={handleHomepageClick}>Ir para a HomePage</button>
         <button onClick={onLoginClick}>Login</button>
       </div>
     </div>
